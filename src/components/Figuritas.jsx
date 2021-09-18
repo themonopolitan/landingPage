da54e6827e00336d59d7b06b9cd18a3bfa76b95d
themonopolitan <themonopolitan@gmail.com>
2021-09-18 12:49:11 -0500

    Arreglamos sintaxis

--- src/components/Figuritas.jsx
+++ src/components/Figuritas.jsx
@@ -5,9 +5,12 @@ const Figuritas = ({ color, position, img, size, padding }) => {
             <div className={`relative h-${size} w-${size} rounded-full overflow-hidden p-${padding} centers`}>
                 <div className={`absolute rounded-full h-full w-full ${color} centers`} />
                 <div className={(img && '') + " sticky h-full w-full rounded-full bg-primary centers"} >
-                    {img && <div className="box-content rounded-full h-5/6 w-5/6 overflow-hidden">
+                    {
+                    img && 
+                    <div className="box-content rounded-full h-5/6 w-5/6 overflow-hidden">
                         <img src={img} alt="" className="h-44" />
-                    </div>}
+                    </div>
+                    }
                 </div>
             </div>
         </div>
