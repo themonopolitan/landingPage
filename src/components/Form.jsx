f8457196b865e093bb268a11225a7d0f2345b65d
themonopolitan <themonopolitan@gmail.com>
2021-09-18 12:49:39 -0500

    Hicimos rework

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,29 +5,23 @@ const Form = () => {
     const inputsInfo = [
         {
             id: 'name',
-            placeholder: 'Tu nombre',
+            placeholder: 'Ingresa tu nombre',
             type: 'text',
             name: 'Nombre'
         },
         {
-            id: 'email',
-            placeholder: 'Tu email',
-            type: 'text',
-            name: 'Correo Electrónico'
-        },
-        {
             id: 'tel',
-            placeholder: 'Tu Whatsapp',
+            placeholder: 'Ingresa tu WhatsApp',
             type: 'tel',
             name: 'Whatsapp'
         },
     ]
 
     return <>
-        <div className="w-2/4 mt-10">
-            <div className="p-6 shadow-xl bg-white w-full flex flex-col justify-between">
-                <span className="center-X mb-3">Regístrare a la Masterclass</span>
-                <div>
+        <div className="lg:w-2/4 mt-10">
+            <div className="w-full p-2 shadow-xl center-Y flex-col justify-between">
+                <span className="center-X mb-6 text-xl">Finaliza tu Registro:</span>
+                <div className= 'lg:w-3/4 w-full'>
                     {
                         inputsInfo.map((info) => <CustomInput
                             id={info.id}
@@ -37,9 +31,9 @@ const Form = () => {
                         />)
                     }
                 </div>
-                <div className="center-X">
-                    <button className="bg-secondary text-white p-8 py-3">
-                        Enviar registro
+                <div className="center-X  lg:w-3/4">
+                    <button className="bg-secondary text-white p-8 py-3 hover:bg-red-400 duration-300">
+                        Completar registro
                     </button>
                 </div>
             </div>
@@ -50,18 +44,20 @@ const Form = () => {
 export const CustomInput = ({ id, placeholder, type, color, solo }) => {
 
     return <>
-        <div className="mx-5 lg:mx-0 grid grid-cols-8 gap-2">
+        <div className={`${solo && 'mx-5' }  lg:mx-0 grid grid-cols-8 gap-2`} >
 
             <input
-                className={`col-span-8 h-12 lg:h-auto lg:col-span-5 bg-transparent border-2 border-gray-400 text-center p-1 text-gray-800 outline-none text-sm ${solo ? 'mb-00' : 'mb-10'}`}
+                className={`col-span-8 h-12 bg-transparent border-2 border-gray-400 text-center p-1 text-gray-800 outline-none text-sm ${solo ? 'lg:h-auto mb-00 lg:col-span-5' : 'mb-6 lg:col-span-8'}`}
                 id={id}
                 placeholder={placeholder}
                 type={type}
             />
-
+        {
+            solo && 
             <button className={`${color} bg-secondary text-white p-3 py-4 text-sm col-span-8 lg:col-span-3 hover:bg-red-400 duration-300 focus:outline-none `}>
                 Apúntate
             </button>
+        }
         </div>
     </>
 }
